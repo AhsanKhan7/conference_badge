dc85af270a9e8f3ca911d15d1678a9c63a7d6b6d
AhsanKhan7 <ahsandev.pro@gmail.com>
2026-01-17 22:56:58 +0500

    Implement Events CRUD with ownership and auth

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,34 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_event, only: [ :show ]
   def index
+    @events = current_user.events.order(date: :desc)
   end
 
   def show
   end
 
   def new
+    @event = current_user.events.new
   end
 
   def create
+    @event = current_user.events.new(event_params)
+
+    if @event.save
+      redirect_to @event, notice: "Event created successfully"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_event
+    @event = current_user.events.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:name, :date, :location)
   end
 end
